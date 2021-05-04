53135af62561030eb128e17d7dcaf394b6e96161
Milena <milena.sof@aol.fr>
2021-05-04 20:50:55 +0200

    Ajout de la vue et du template de détail blog post ainsi que du CSS pour le site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views #importation de toutes les vues de l'application blog
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
